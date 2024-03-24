86f2e1cb08f53a49246ad650fee74c88ee244240
Fluent Bridge <128307875+huzaifaAmeer02@users.noreply.github.com>
2024-03-24 20:25:17 +0530

    Delete FluentBridge-Server directory

--- FluentBridge-Server/SignupBackend/route/QuestionRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const Question = require("../controller/QuestionController");
-
-
-const router = express.Router();
-router.get('/get_questions', Question.getAllQuestions);
-
-module.exports=router;
